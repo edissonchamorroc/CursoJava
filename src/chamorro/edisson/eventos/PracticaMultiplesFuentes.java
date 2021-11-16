6e1aada8ba2503c383c1a8c3310d26ff820342d4
edisson <john.chamorro@udea.edu.co>
2021-11-15 19:32:10 -0500

    Se adiciona clase de multiples fuentes

--- src/chamorro/edisson/eventos/PracticaMultiplesFuentes.java
+++ src/chamorro/edisson/eventos/PracticaMultiplesFuentes.java
@@ -4,8 +4,13 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 import chamorro.edisson.interfacesgraficas.GenericFrame;
 
@@ -14,9 +19,9 @@ public class PracticaMultiplesFuentes {
 	public static void main(String[] args) {
 
 		LaminaFuenteBotones laminaBotones = new LaminaFuenteBotones();
-		laminaBotones.setBotones("Azul", Color.BLUE);
-		laminaBotones.setBotones("Rojo", Color.RED);
-		laminaBotones.setBotones("Verde", Color.GREEN);
+		laminaBotones.setBotones("Azul", Color.BLUE,"ctrl a");
+		laminaBotones.setBotones("Rojo", Color.RED,"ctrl r");
+		laminaBotones.setBotones("Verde", Color.GREEN,"ctrl g");
 		GenericFrame frameMaster = new GenericFrame(3, 400, 100, 500, 400, laminaBotones);
 
 	}
@@ -25,10 +30,22 @@ public class PracticaMultiplesFuentes {
 
 class LaminaFuenteBotones extends JPanel {
 
-	public void setBotones(String nombreBoton, Color color) {
+	public void setBotones(String nombreBoton, Color color, String comando) {
 		JButton button = new JButton(nombreBoton);
-		button.addActionListener(new EventoCambiarColorBack(nombreBoton, color));
+		
+		EventoCambiarColorBack evento = new EventoCambiarColorBack(nombreBoton, color);
+		
+		button.addActionListener(evento);
+		
 		add(button);
+		
+		InputMap mapaEntrada = getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+			
+		mapaEntrada.put(KeyStroke.getKeyStroke(comando), nombreBoton);
+		
+		ActionMap mapaAccion = getActionMap();
+		
+		mapaAccion.put(nombreBoton, evento );
 
 	}
 
